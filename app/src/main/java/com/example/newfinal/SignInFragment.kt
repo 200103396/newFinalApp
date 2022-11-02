e57c718a411832c9dd39d336011f37ca9a2b3cd6
200103396 <91262551+200103396@users.noreply.github.com>
2022-11-03 02:44:19 +0600

    Akbo added Menu

--- app/src/main/java/com/example/newfinal/SignInFragment.kt
+++ app/src/main/java/com/example/newfinal/SignInFragment.kt
@@ -1,6 +1,22 @@
 package com.example.newfinal
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.newfinal.databinding.SignInBinding
 
 class SignInFragment: Fragment(R.layout.sign_in) {
+
+    private lateinit var binding: SignInBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = SignInBinding.bind(view)
+        binding.signInButton.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_options_fragment2)
+        }
+    }
 }
