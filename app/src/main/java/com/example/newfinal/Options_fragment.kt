93bb368fcf2ebfad893c8486b3b6537592edab81
200103396 <91262551+200103396@users.noreply.github.com>
2022-11-08 22:30:04 +0600

    fixing list adding vm

--- app/src/main/java/com/example/newfinal/Options_fragment.kt
+++ app/src/main/java/com/example/newfinal/Options_fragment.kt
@@ -12,21 +12,7 @@ import com.example.newfinal.databinding.OptionsGenreBinding
 
 class Options_fragment : Fragment(R.layout.options_genre)  {
 
-    lateinit var elements : ArrayList<ListElement>;
     private lateinit var binding: OptionsGenreBinding
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<ListAdapter.ItemViewHolder>? = null
-
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//
-//
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.cards_element, container, false)
-//    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/newfinal/Recycle_fragment.kt
+++ app/src/main/java/com/example/newfinal/Recycle_fragment.kt
@@ -4,22 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newfinal.databinding.CardsElementBinding
+import com.example.newfinal.databinding.ListElementBinding
 
 class Recycle_fragment : Fragment(R.layout.cards_element) {
 
     lateinit var elements: ArrayList<ListElement>
     private lateinit var binding: CardsElementBinding
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = CardsElementBinding.bind(view)
         init()
+
+        binding.poka4to.setOnClickListener{
+            findNavController().navigate(R.id.action_recycle_fragment_to_pageFragment)
+        }
+
+
     }
 
+    fun next(){
+        lateinit var binding: ListElementBinding
+        binding.selectBook.setOnClickListener{
+            findNavController().navigate(R.id.action_recycle_fragment_to_pageFragment)
+        }
+    }
     fun init(): Unit {
         elements = ArrayList<ListElement>();
         elements.add(ListElement("#775447", "Harry Potter", "Joanne Rowling", "Favorite"))
